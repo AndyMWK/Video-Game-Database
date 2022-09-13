2d077d8945be5876a8f6a7cc0892732af0b1b4ce
AndyMWK <111655912+AndyMWK@users.noreply.github.com>
2022-09-12 20:09:17 -0700

    Update a5.cpp

--- a5.cpp
+++ a5.cpp
@@ -6,29 +6,6 @@
 
 // a5.cpp
 
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
-// Name : <Min Woo (Andy) Kim>
-// St.# : <301448177>
-// Email: <andy_kim_10@sfu.ca>
-//
-//
-// Statement of Originality
-// ------------------------
-//
-// All the code and comments below are my own original work. For any non-
-// original work, I have provided citations in the comments with enough
-// detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, I have not shared this work with anyone else, and I have
-// not seen solutions from other students, tutors, websites, books,
-// etc.
-//
-
 using namespace std;
 
 //basic function for listing items
